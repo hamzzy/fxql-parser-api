d4aa3421124596be59a02866b546a7c361cb5989
hamzzy <hamat.ibrahim3@gmail.com>
2024-11-20 00:34:00 +0100

    fix: fix test failing on CI

--- src/fxql/fxql.controller.ts
+++ src/fxql/fxql.controller.ts
@@ -1,15 +1,10 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
 } from '@nestjs/common';
 import { FxqlService } from './fxql.service';
 import { CreateFxqlDto } from './dto/create-fxql.dto';
-import { UpdateFxqlDto } from './dto/update-fxql.dto';
 
 @Controller('fxql')
 export class FxqlController {
@@ -18,25 +13,5 @@ export class FxqlController {
   @Post()
   create(@Body() createFxqlDto: CreateFxqlDto) {
     return this.fxqlService.create(createFxqlDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.fxqlService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.fxqlService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFxqlDto: UpdateFxqlDto) {
-    return this.fxqlService.update(+id, updateFxqlDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.fxqlService.remove(+id);
   }
 }
